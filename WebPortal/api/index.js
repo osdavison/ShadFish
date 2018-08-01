4b87b092e4d28a088da78aa94fe9570b9b0358b4
Seth Davison <seth@davison.net>
2018-08-01 17:55:10 -0400

    Refactor client side to allow user to submit a move

--- WebPortal/api/index.js
+++ WebPortal/api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 var moves =
-  "e4 e5 Nf3 f6 Nxe5 fxe5 Qh5+ Ke7 Qxe5+ Kf7 Bc4+ d5 Bxd5+ Kg6 h4 h5 Bxb7 Bxb7 Qf5+ Kh6 d4+ g5 Qf7 Qe7 hxg5+ Qxg5 Rxh5";
+  "e4 e5 Nf3 f6 Nxe5 fxe5 Qh5+ Ke7 Qxe5+ Kf7 Bc4+ d5 Bxd5+ Kg6 h4 h5 Bxb7 Bxb7 Qf5+ Kh6 d4+ g5 Qf7 Qe7 hxg5+ Qxg5";
 
 router.get("/moves", (req, res) => {
   res.send({
--- WebPortal/src/components/App.js
+++ WebPortal/src/components/App.js
@@ -1,52 +1,99 @@
 import React from "react";
-import { Chessboard, COLOR, MOVE_INPUT_MODE } from "cm-chessboard";
+import {
+  Chessboard,
+  COLOR,
+  MOVE_INPUT_MODE,
+  INPUT_EVENT_TYPE
+} from "cm-chessboard";
 import Chess from "chess.js";
 import axios from "axios";
 
 class App extends React.Component {
-  componentDidMount() {
-    var chessboardProps = {
-      position: "start", // set as fen, "start" or "empty"
-      orientation: COLOR.white, // white on bottom
-      style: {
-        cssClass: "default",
-        showCoordinates: true, // show ranks and files
-        showBorder: false // display a border around the board
-      },
-      responsive: false, // resizes the board on window resize, if true
-      animationDuration: 300, // pieces animation duration in milliseconds
-      moveInputMode: MOVE_INPUT_MODE.dragMarker, // set to MOVE_INPUT_MODE.dragPiece or MOVE_INPUT_MODE.dragMarker for interactive movement
-      sprite: {
-        url: "./chessboard-sprite.svg",
-        grid: 40 // the sprite is tiled with one piece every 40px
-      }
-    };
+  chess = new Chess();
+  chessboard = void 0;
+  moves = [];
 
-    var chessboard = new Chessboard(
+  componentDidMount() {
+    this.chessboard = new Chessboard(
       document.getElementById("chessboard-container"),
-      chessboardProps
+      {
+        position: "start", // set as fen, "start" or "empty"
+        orientation: COLOR.white, // white on bottom
+        style: {
+          cssClass: "default",
+          showCoordinates: true, // show ranks and files
+          showBorder: false // display a border around the board
+        },
+        responsive: false, // resizes the board on window resize, if true
+        animationDuration: 300, // pieces animation duration in milliseconds
+        moveInputMode: MOVE_INPUT_MODE.dragMarker, // set to MOVE_INPUT_MODE.dragPiece or MOVE_INPUT_MODE.dragMarker for interactive movement
+        sprite: {
+          url: "./chessboard-sprite.svg",
+          grid: 40 // the sprite is tiled with one piece every 40px
+        }
+      }
     );
-    var chess = new Chess();
+
+    this.getMoves().then(moves => {
+      this.moves = moves;
+      this.applyMoves(moves);
+    });
 
     setInterval(() => {
-      chess.reset();
-
-      axios.get("/api/moves").then(
-        response => {
-          var moves = response.data.moves.split(" ");
-
-          moves.forEach(move => {
-            chess.move(move);
-          });
-
-          chessboard.setPosition(chess.fen());
-        },
-        () => {
-          console.error("Could not get moves");
+      this.getMoves().then(moves => {
+        if (this.moves !== moves) {
+          this.moves = moves;
+          this.applyMoves(moves);
         }
-      );
+      });
     }, 2000);
+
+    this.chessboard.enableMoveInput(this.handleMoveInput, COLOR.white);
   }
+
+  getMoves = () =>
+    axios.get("/api/moves").then(response => {
+      console.log(response.data);
+
+      return response.data.moves.split(" ");
+    });
+
+  applyMoves = moves => {
+    this.chess.reset();
+
+    moves.forEach(move => {
+      this.chess.move(move);
+    });
+
+    this.chessboard.setPosition(this.chess.fen());
+  };
+
+  handleMoveInput = event => {
+    switch (event.type) {
+      case INPUT_EVENT_TYPE.moveStart:
+        console.log(`moveStart: ${event.square}`);
+        if (this.chess.game_over()) {
+          return false;
+        }
+        return true;
+      case INPUT_EVENT_TYPE.moveDone:
+        var moveResult = this.chess.move({
+          from: event.squareFrom,
+          to: event.squareTo
+        });
+        if (!moveResult) {
+          return false;
+        }
+
+        this.moves.push(moveResult.san);
+
+        axios.post("/api/moves", { moves: this.moves.join(" ") });
+
+        return true;
+      case INPUT_EVENT_TYPE.moveCanceled:
+        console.log(`moveCanceled`);
+    }
+  };
 
   render() {
     try {
