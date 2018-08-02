bed47c462ebac5a8d7e12ed008ea53d4a7f36819
Seth Davison <seth@davison.net>
2018-08-02 15:37:41 -0400

    Start writing SQL queries

--- OraclePortal/oracleController.ts
+++ OraclePortal/oracleController.ts
@@ -28,20 +28,33 @@ router.post("/moves", async (req: Request, res: Response) => {
       return res.status(500).send("DB connection failed");
     }
 
-    const sqlCommand = "TODO: SQL command";
-
-    connection.execute(sqlCommand, [103], (err, result) => {
+    const sqlSelectCommand = "SELECT id FROM shadfish";
+    connection.execute(sqlSelectCommand, (err, result) => {
       if (err) {
         console.error(err.message);
-        if (connection) {
-          doRelease(connection);
-        }
+      }
+      console.log(result);
+
+      // If no rows or last row is a completed game, commit moves using INSERT with an ID one higher than that of the last row
+      // Else commit moves using UPDATE and the ID of the last row
+    });
+
+    const sqlInsertCommand = `INSERT INTO shadfish (id, moves, column3) VALUES (1, ${
+      req.body.moves
+    }, foo);`;
+
+    const sqlUpdateCommand = `UPDATE shadfish
+                              SET id = 1, moves = ${
+                                req.body.moves
+                              }, column3 = foo
+                              WHERE id = 1`;
+
+    connection.execute(sqlInsertCommand, (err, result) => {
+      if (err) {
+        console.error(err.message);
         return;
       }
       console.log(result.rows);
-      if (connection) {
-        doRelease(connection);
-      }
     });
   } catch (err) {
     return res.status(500).send(err);
