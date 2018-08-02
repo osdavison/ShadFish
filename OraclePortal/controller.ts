c02ef8e68253243bb52560d45ce9993a8032f8b1
Seth Davison <seth@davison.net>
2018-08-02 11:31:35 -0400

    Add dependencies and create route to commit moves to oracle

--- OraclePortal/controller.ts
+++ OraclePortal/controller.ts
@@ -1,9 +1,51 @@
 import { Router, Request, Response } from "express";
+import * as oracledb from "oracledb";
+import { IConnection } from "oracledb";
 
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+const createConnection = () => {
+  return oracledb.getConnection({
+    user: "shadfish",
+    password: "iti001",
+    connectString: "localhost/XE"
+  });
+};
+
+const doRelease = (connection: IConnection) => {
+  connection.close(function(err) {
+    if (err) console.error(err.message);
+  });
+};
+
+router.post("/moves", async (req: Request, res: Response) => {
+  console.log(req.body);
+
+  try {
+    const connection = await createConnection();
+
+    if (!connection) {
+      return res.status(500).send("DB connection failed");
+    }
+
+    const sqlCommand = "TODO: SQL command";
+
+    connection.execute(sqlCommand, [103], (err, result) => {
+      if (err) {
+        console.error(err.message);
+        if (connection) {
+          doRelease(connection);
+        }
+        return;
+      }
+      console.log(result.rows);
+      if (connection) {
+        doRelease(connection);
+      }
+    });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 });
 
 export default router;
--- OraclePortal/server.ts
+++ OraclePortal/server.ts
@@ -1,8 +1,11 @@
 import * as express from "express";
 import router from "./controller";
+import * as bodyParser from "body-parser";
 
 const app: express.Application = express();
 const port: number = 3000;
+
+app.use(bodyParser.json());
 
 app.use("/", router);
 
